f2e073752a881d051930896d29145584fc5a7a30
adamlhardy <adam.hardy37@gmail.com>
2024-04-23 10:41:25 -0700

    feat: added BinarySearchTree and Node

---
+++ Node.java
@@ -0,0 +1,29 @@
+public class Node {
+
+    /**
+     * The data or value that the Node contains
+     */
+    int data;
+
+    /**
+     * Pointer to the left Node - which is smaller than the parent
+     */
+    Node left;
+    
+    /**
+     * Pointer to the right Node 
+     * which is greater than or equal to the parent
+     */
+    Node right;
+
+    /**
+     * Constructor that takes in data as a parameter and 
+     * sets it to the instance variable
+     * @param data the int data that should be set
+     */
+    public Node(int data) {
+        super();
+        this.data = data;
+    }
+    
+}
